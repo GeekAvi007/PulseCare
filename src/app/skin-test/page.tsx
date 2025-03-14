29041ac19765175b936d02451176622f688d12ba
Avishek <avishekmukherje007@gmail.com>
2025-03-14 16:04:43 +0530

    Brain Tumor and Skin Disease Mock setups done

---
+++ src/app/skin-test/page.tsx
@@ -0,0 +1,9 @@
+import SkinTestPage from "@/components/SkinTest";
+
+export default function SkinTest() {
+  return(
+    <div className="mt-30 justify-center w-full">
+   <SkinTestPage />
+   </div>
+  )
+}
